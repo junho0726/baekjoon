8c9e909b1cfdd93b24e4f6a42618c340b755d231
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-01-20 20:51:57 +0900

    Update P2557.java

--- 단계_01_입출력과_사칙연산/P2557.java
+++ 단계_01_입출력과_사칙연산/P2557.java
@@ -1,1 +1,11 @@
+package 단계_01_입출력과_사칙연산;
 
+/**
+ * 문제 Hello World!를 출력하시오.
+ * 출력 Hello World!를 출력하시오.
+ */
+public class P2557 {
+    public static void main (String args []) {
+        System.out.println("Hello World!");
+    }
+}
