1f579f375c081d99f760668339bc773fa567d3bc
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-01-20 20:54:17 +0900

    Create P10869.java

---
+++ 단계_01_입출력과_사칙연산/P10869.java
@@ -0,0 +1,29 @@
+package 단계_01_입출력과_사칙연산;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 문제 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+ * 입력 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+ * 출력 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+ */
+public class P10869 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(a+b).append('\n');
+        sb.append(a-b).append('\n');
+        sb.append(a*b).append('\n');
+        sb.append(a/b).append('\n');
+        sb.append(a%b).append('\n');
+
+        System.out.print(sb);
+    }
+}
