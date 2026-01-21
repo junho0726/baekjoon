d349a45eb25febb37aa2bb473a9e467554b3f760
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-01-21 18:57:07 +0900

    Update P1001.java

--- 단계_01_입출력과_사칙연산/P1001.java
+++ 단계_01_입출력과_사칙연산/P1001.java
@@ -3,6 +3,11 @@ package 단계_01_입출력과_사칙연산;
 import java.io.*;
 import java.util.*;
 
+/**
+ * 문제 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ * 입력 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * 출력 첫째 줄에 A-B를 출력한다.
+ */
 public class P1001 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
