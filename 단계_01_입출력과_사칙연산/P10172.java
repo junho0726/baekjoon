0755390858a46a51bb52b4d0583527593f9fd356
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-01-21 20:27:18 +0900

    Create P10172.java

---
+++ 단계_01_입출력과_사칙연산/P10172.java
@@ -0,0 +1,16 @@
+package 단계_01_입출력과_사칙연산;
+
+/**
+ * 문제 아래 예제와 같이 개를 출력하시오.
+ * 입력 없음.
+ * 출력 개를 출력한다.
+ */
+public class P10172 {
+    public static void main(String args[]){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.print("||_/=\\\\__|");
+    }
+}
