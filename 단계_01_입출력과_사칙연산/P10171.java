e93348d8ba0081a5f119e6988a643b4c3572328f
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-01-21 20:26:33 +0900

    Create P10171.java

---
+++ 단계_01_입출력과_사칙연산/P10171.java
@@ -0,0 +1,15 @@
+package 단계_01_입출력과_사칙연산;
+
+/**
+ * 문제 아래 예제와 같이 고양이를 출력하시오.
+ * 입력 없음.
+ * 출력 고양이를 출력한다.
+ */
+public class P10171 {
+    public static void main(String args[]){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
