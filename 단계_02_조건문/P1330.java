c105b217dc63aa10253504f72ece7f9ca232a80f
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-01-22 21:05:59 +0900

    Create P1330.java

---
+++ 단계_02_조건문/P1330.java
@@ -0,0 +1,30 @@
+package 단계_02_조건문;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+ * 입력 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
+ * 출력 첫째 줄에 다음 세 가지 중 하나를 출력한다.
+ * A가 B보다 큰 경우에는 '>'를 출력한다.
+ * A가 B보다 작은 경우에는 '<'를 출력한다.
+ * A와 B가 같은 경우에는 '=='를 출력한다.
+ */
+public class P1330 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if(a > b) {
+            System.out.print(">");
+        }else if(a < b) {
+            System.out.print("<");
+        }else {
+            System.out.print("==");
+        }
+    }
+}
