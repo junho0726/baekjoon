52beb47ca2f02640f01ed18d3b78ddd0bb373214
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-01-26 20:52:29 +0900

    Create P10807.java

---
+++ 단계_04_1차원_배열/P10807.java
@@ -0,0 +1,33 @@
+package 단계_04_1차원_배열;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제
+ * 총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
+ * 입력
+ * 첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 정수가 공백으로 구분되어져있다. 셋째 줄에는 찾으려고 하는 정수 v가 주어진다. 입력으로 주어지는 정수와 v
+ * 는 -100보다 크거나 같으며, 100보다 작거나 같다.
+ * 출력
+ * 첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
+ *
+ */
+public class P10807 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        for(int i = 0; i < num; i++) {
+            int result = Integer.parseInt(st.nextToken());
+            if(result == b) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
+}
