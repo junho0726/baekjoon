4eb095cb07c423290144af50ec6ed255ac21f6d1
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-02-02 21:04:25 +0900

    Create P11720.java

---
+++ 단계_05_문자열/P11720.java
@@ -0,0 +1,26 @@
+package 단계_05_문자열;
+
+import java.io.*;
+
+/**
+ * 문제
+ * N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+ * 입력
+ * 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+ * 출력
+ * 입력으로 주어진 숫자 N개의 합을 출력한다.
+ *
+ */
+public class P11720 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            char m = s.charAt(i);
+            sum += m - '0';
+        }
+        System.out.println(sum);
+    }
+}
