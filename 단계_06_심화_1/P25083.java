f6c0c00afb96f123668a4e0c1a6ed45642cede7c
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-02-05 20:49:49 +0900

    Create P25083.java

---
+++ 단계_06_심화_1/P25083.java
@@ -0,0 +1,21 @@
+package 단계_06_심화_1;
+
+/**
+ * 문제
+ * 아래 예제와 같이 새싹을 출력하시오.
+ * 입력
+ * 입력은 없다.
+ * 출력
+ * 새싹을 출력한다.
+ */
+public class P25083 {
+    public static void main(String args []) {
+        System.out.println("         ,r'\"7\n" +
+                           "r`-_   ,'  ,/\n" +
+                           " \\. \". L_r'\n" +
+                           "   `~\\/\n" +
+                            "      |\n" +
+                            "      |"
+        );
+    }
+}
