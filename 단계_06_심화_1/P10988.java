06b0afaccd184c60a2ceeb0a023dcef9d07b0f76
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-02-06 20:25:12 +0900

    Create P10988.java

---
+++ 단계_06_심화_1/P10988.java
@@ -0,0 +1,30 @@
+package 단계_06_심화_1;
+
+import java.io.*;
+
+/**
+ * 문제
+ * 알파벳 소문자로만 이루어진 단어가 주어진다. 이때, 이 단어가 팰린드롬인지 아닌지 확인하는 프로그램을 작성하시오.
+ * 팰린드롬이란 앞으로 읽을 때와 거꾸로 읽을 때 똑같은 단어를 말한다.
+ * level, noon은 팰린드롬이고, baekjoon, online, judge는 팰린드롬이 아니다.
+ * 입력
+ * 첫째 줄에 단어가 주어진다. 단어의 길이는 1보다 크거나 같고, 100보다 작거나 같으며, 알파벳 소문자로만 이루어져 있다.
+ * 출력
+ * 첫째 줄에 팰린드롬이면 1, 아니면 0을 출력한다.
+ */
+public class p10988 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s =  br.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        String reverse = sb.append(s).reverse().toString();
+
+        if(s.equals(reverse)){
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
