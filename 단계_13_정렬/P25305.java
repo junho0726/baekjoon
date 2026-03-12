674a790794ccd2675ff390cea9152ddde0bc5e46
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-03-12 20:43:56 +0900

    Create P25305.java

---
+++ 단계_13_정렬/P25305.java
@@ -0,0 +1,36 @@
+package 단계_13_정렬;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제
+ * 2022 연세대학교 미래캠퍼스 슬기로운 코딩생활에 N명의 학생들이 응시했다.
+ * 이들 중 점수가 가장 높은 k명은 상을 받을 것이다. 이 때, 상을 받는 커트라인이 몇 점인지 구하라.
+ *
+ * 커트라인이란 상을 받는 사람들 중 점수가 가장 낮은 사람의 점수를 말한다.
+ *
+ * 입력
+ * 첫째 줄에는 응시자의 수 N과 상을 받는 사람의 수 k가 공백을 사이에 두고 주어진다.
+ * 둘째 줄에는 각 학생의 점수 x가 공백을 사이에 두고 주어진다.
+ *
+ * 출력
+ * 상을 받는 커트라인을 출력하라.
+ */
+public class P25305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int [] arr = new int [N];
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[N-K]);
+    }
+}
