708b47dd5b0b51abaf4fcd1749e97b9d8ec80d78
junho0726 <65691165+junho0726@users.noreply.github.com>
2026-03-18 20:43:34 +0900

    Create P11651.java

---
+++ 단계_13_정렬/P11651.java
@@ -0,0 +1,46 @@
+package 단계_13_정렬;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제
+ * 2차원 평면 위의 점 N개가 주어진다.
+ * 좌표를 y좌표가 증가하는 순으로,
+ * y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다.
+ * 둘째 줄부터 N개의 줄에는 i번 점의 위치 xi와 yi가 주어진다.
+ * (-100,000 ≤ xi, yi ≤ 100,000)
+ * 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+ *
+ * 출력
+ * 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+ */
+public class P11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int [][] arr = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if(a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i][0]+" " + arr[i][1]);
+        }
+
+    }
+}
